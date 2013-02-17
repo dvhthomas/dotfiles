7ce1372641182a4abcd227d5bcc1810e7a783c7a
Dylan Thomas <dvhthomas@gmail.com>
2013-02-17 13:12:22 -0800

    Adding ctrlp, jedi, vim-sensible plugins, zapping ultisnips

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# run this from the repo directory, e.g., /home/you/projects/dotfiles
+# Assumes that you've clone the dotfiles repo into $HOME/dotfiles
 rm ~/.vimrc
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 rm -rf ~/.vim
@@ -7,3 +7,13 @@ rm ~/.gitconfig
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 rm ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+# Jedi code completion plugin for vim needs this
+# python library
+pip install jedi
+
+# All vim plugins managed as Git submodules. Grab the lot
+cd ~/dotfiles
+git submodule update --init --recursive
+git submodule update --recursive
+cd ~
