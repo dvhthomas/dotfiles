d295952fec84da2e6d9467cb44a2b49f0d6a6d36
Dylan Thomas <dvhthomas@gmail.com>
2013-02-18 16:26:58 -0800

    Tweaking for vundle usage

--- install.sh
+++ install.sh
@@ -8,12 +8,18 @@ ln -s ~/dotfiles/gitconfig ~/.gitconfig
 rm ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
+# Managing all bundles with vundle...
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# Put any prerequisites for bundles
 # Jedi code completion plugin for vim needs this
 # python library
 pip install jedi
 
+# Install the bundles
+vim +BundleInstall
+
 # All vim plugins managed as Git submodules. Grab the lot
 cd ~/dotfiles
-git submodule update --init --recursive
-git submodule update --recursive
+#git submodule update --init --recursive
+#git submodule update --recursive
 cd ~
