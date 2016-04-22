071176fdff2ead83ccf66e788fde4adf5021517d
Dylan Thomas <dylanthomas@google.com>
2016-04-22 08:30:13 -0700

    Removing git config

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ rm ~/.vimrc
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 rm -rf ~/.vim
 ln -s ~/dotfiles/vim ~/.vim
-rm ~/.gitconfig
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
+#rm ~/.gitconfig
+#ln -s ~/dotfiles/gitconfig ~/.gitconfig
 rm ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 rm ~/.tmux-osx.conf
